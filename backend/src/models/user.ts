f03c55f2f1b8593700f65d5343d2cd431684dd5c
ThomasNakas <thomasnakas92@gmail.com>
2024-02-16 01:48:11 +0200

    essagetrying debugging the login and register process

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,19 +1,19 @@
 import mongoose from "mongoose";
-
-
-export type UserType = {
-    _id : string;
-    email: string;
-    password: string;
-    firstName: string;
-    lastName: string;
-};
+import bcrypt from "bcryptjs";
+import { UserType } from "../shared/types";
 
 const userSchema = new mongoose.Schema({
-    email: {type: String, required: true, unique: true},
-    password: {type: String, required: true},
-    firstName:{type: String, required: true},
-    lastName:{type: String, required: true}
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
+  next();
 });
 
 const User = mongoose.model<UserType>("User", userSchema);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,9 +5,20 @@ import {
   Navigate,
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
-
+import Register from "./pages/Register";
+import SignIn from "./pages/SignIn";
+import AddHotel from "./pages/AddHotel";
+import { useAppContext } from "./contexts/AppContext";
+import MyHotels from "./pages/MyHotels";
+import EditHotel from "./pages/EditHotel";
+import Search from "./pages/Search";
+import Detail from "./pages/Detail";
+import Booking from "./pages/Booking";
+import MyBookings from "./pages/MyBookings";
+import Home from "./pages/Home";
 
 const App = () => {
+  const { isLoggedIn } = useAppContext();
   return (
     <Router>
       <Routes>
@@ -15,7 +26,7 @@ const App = () => {
           path="/"
           element={
             <Layout>
-              <p>Home Page</p>
+              <Home />
             </Layout>
           }
         />
@@ -23,7 +34,15 @@ const App = () => {
           path="/search"
           element={
             <Layout>
-              <p>Search Page</p>
+              <Search />
+            </Layout>
+          }
+        />
+        <Route
+          path="/detail/:hotelId"
+          element={
+            <Layout>
+              <Detail />
             </Layout>
           }
         />
@@ -31,7 +50,7 @@ const App = () => {
           path="/register"
           element={
             <Layout>
-              <p>Register Page</p>
+              <Register />
             </Layout>
           }
         />
@@ -39,10 +58,56 @@ const App = () => {
           path="/sign-in"
           element={
             <Layout>
-              <p>Sign In Page</p>
+              <SignIn />
             </Layout>
           }
         />
+
+        {isLoggedIn && (
+          <>
+            <Route
+              path="/hotel/:hotelId/booking"
+              element={
+                <Layout>
+                  <Booking />
+                </Layout>
+              }
+            />
+
+            <Route
+              path="/add-hotel"
+              element={
+                <Layout>
+                  <AddHotel />
+                </Layout>
+              }
+            />
+            <Route
+              path="/edit-hotel/:hotelId"
+              element={
+                <Layout>
+                  <EditHotel />
+                </Layout>
+              }
+            />
+            <Route
+              path="/my-hotels"
+              element={
+                <Layout>
+                  <MyHotels />
+                </Layout>
+              }
+            />
+            <Route
+              path="/my-bookings"
+              element={
+                <Layout>
+                  <MyBookings />
+                </Layout>
+              }
+            />
+          </>
+        )}
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
