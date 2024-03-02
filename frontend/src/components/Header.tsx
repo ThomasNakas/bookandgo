0990438123997bb46be61f852a6c8e395c2f2cb4
ThomasNakas <thomasnakas92@gmail.com>
2024-03-02 04:56:39 +0200

    restoring my src and cleaning my directories

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import { useAppContext } from "../contexts/AppContext";
+import SignOutButton from "./SignOutButton";
+
+const Header = () => {
+  const { isLoggedIn } = useAppContext();
+
+  return (
+    <div className="bg-pink-500 py-6">
+      <div className="container mx-auto flex justify-between">
+        <span className="text-3xl text-white font-bold tracking-tight">
+          <Link to="/">BookAndGo.com</Link>
+        </span>
+        <span className="flex space-x-2">
+          {isLoggedIn ? (
+            <>
+              <Link
+                className="flex items-center text-white px-3 font-bold hover:bg-pink-200"
+                to="/my-bookings"
+              >
+                My Bookings
+              </Link>
+              <Link
+                className="flex items-center text-white px-3 font-bold hover:bg-pink-200"
+                to="/my-hotels"
+              >
+                My Hotels
+              </Link>
+              <SignOutButton />
+            </>
+          ) : (
+            <Link
+              to="/sign-in"
+              className="flex bg-white items-center text-pink-500 px-3 font-bold hover:bg-gray-100"
+            >
+              Sign In
+            </Link>
+          )}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
